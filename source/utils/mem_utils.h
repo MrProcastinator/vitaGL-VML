ce478d14b308604551bc2041d45eb6024cf20a46
MrProcastinator <mrprocastinator@tutanota.com>
2025-08-12 00:16:21 -0300

    Fix reference to define macro when compiling without clib

--- source/utils/mem_utils.h
+++ source/utils/mem_utils.h
@@ -29,7 +29,7 @@
 // Debug flags
 //#define DEBUG_GC // Enable this to enable logging for the garbage collector
 
-#ifdef NO_CLIB
+#ifdef DISABLE_CLIB
 #define vgl_memset memset
 #define vgl_fast_memcpy memcpy
 #else
